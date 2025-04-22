fc58a7ba8015b91222a75bc9a4c3041585002a91
Gitbottelegramserver <faton1234567891@gmail.com>
2025-04-22 20:55:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from aiogram import executor
+import asyncio
+from aiogram import Bot, Dispatcher
+from app.dialogs import router  # убедись, что router есть в dialogs.py
+from app.config import TOKEN  # токен из config
 
-from app import bot
+# Инициализация бота
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+dp.include_router(router)  # добавляем маршруты (если есть)
 
+# Асинхронный запуск
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
-executor.start_polling(bot.dp,
-                       skip_updates=True,
-                       on_shutdown=bot.on_shutdown)
+# Точка входа
+if __name__ == "__main__":
+    asyncio.run(main())
+
